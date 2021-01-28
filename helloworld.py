68af88ed38bf4fb6b2dadf631168bc773ca264ab
Ezio Lemes <eziolemes@gmail.com>
2021-01-27 21:36:19 -0400

    Teste de chave ssh

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+if 5 > 2:
+    print("5 é maior que 2!")
+
+#Declaração de variáveis
+x = 5
+y = "Hello Python"
+print(x)
+print(y)
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+    #casting
+    a = str(3)   # será '3'
+    z = int(3)   # será 3
+    j = float(3) # z será 3.0
